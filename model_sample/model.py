63c641e277d456e8e702c4fb6e145839c0b12ef0
Krito-linux <iroandzzy@gmail.com>
2025-11-06 17:48:52 +0800

    master

--- model_sample/model.py
+++ model_sample/model.py
@@ -1,4 +1,8 @@
 import torch.nn as nn
+import torch.optim as optim
+
+
+
 '''
 注意传入参数，主要是输入的层数和输出的动作
 '''
@@ -6,6 +10,8 @@ class Model(nn.modules):
     
     def __init__(self):
         super(Model,self).__init__()
+        pass
+    
 
     def forward(self,x):
         return x
